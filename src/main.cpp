9d56a83714af42ed24e1e0b6f1657e58260c4f6e
SystemProgrammerWizzard <tatandat110105@gmail.com>
2025-11-07 19:47:11 -0500

    add option parsing and improve verbose output
    Signed-off-by: SystemProgrammerWizzard <tatandat110105@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include "CLI.hpp"
+#include <vector>
+#include <string>
 
 int main(int argc, char* argv[]) {
     CLI::App app{"HyperShell CLI"};
 
     bool verbose = false;
+    std::vector<std::string> commands;
     app.add_flag("-v,--verbose", verbose, "Verbose mode");
     app.set_version_flag("-V,--version", "HyperShell v0.1.0");
 
-    CLI11_PARSE(app,argc, argv);
+    app.add_option("commands", commands, "Commands to execute")->expected(0, -1);
 
-    if (verbose) std::cout << "Verbose mode enables\n";
+    if (argc == 1) {
+        std::cout << app.help() << std::endl;
+        return 0;
+    }
 
-    // Use cli.cmds() as your commands
+    CLI11_PARSE(app, argc, argv);
+  
+
+    if (verbose){
+        std::cout << "[Verbose] Parsed" << commands.size() << "command(s)" << std::endl;
+    }
+    for (size_t i = 0; i < commands.size(); i++){
+        std::cout << "Command #" << i+1 << commands[i] << std::endl; 
+    }
+
+    
     return 0;
 }
