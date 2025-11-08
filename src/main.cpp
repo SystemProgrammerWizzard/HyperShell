aaa69bf603b0e3e374641da04de05cc1225a329e
SystemProgrammerWizzard <tatandat110105@gmail.com>
2025-11-07 19:32:54 -0500

    Refactor command line handling: replace custom implementation with CLI11 library
    Signed-off-by: SystemProgrammerWizzard <tatandat110105@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-#include "utils/command_line_options.hpp"
+#include "CLI.hpp"
 
 int main(int argc, char* argv[]) {
-    CommandLineOptions cli;
-    int status = cli.parse(argc, argv);
-    if (status < 0) return 1;  // usage error
-    if (status == 0 && (cli.opts().help || cli.opts().version)) return 0; // already printed & done
+    CLI::App app{"HyperShell CLI"};
 
-    // Normal run:
-    if (cli.opts().verbose) {
-        std::cout << "Executing with verbose mode\n";
-    } else {
-        std::cout << "Run without verbose mode\n";
-        for (const auto& element: cli.cmd()){
-            std::cout << element << std::endl;
-        }
-    }
+    bool verbose = false;
+    app.add_flag("-v,--verbose", verbose, "Verbose mode");
+    app.set_version_flag("-V,--version", "HyperShell v0.1.0");
+
+    CLI11_PARSE(app,argc, argv);
+
+    if (verbose) std::cout << "Verbose mode enables\n";
 
     // Use cli.cmds() as your commands
     return 0;
